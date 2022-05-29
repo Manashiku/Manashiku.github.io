3084643f1ebad073e7504d7c01dc8d8cb6e31711
Manashiku <46613923+Manashiku@users.noreply.github.com>
2022-05-29 12:00:57 -0700

    Add files via upload

---
+++ eval.js
@@ -0,0 +1,645 @@
+var chr_list = [
+    {
+        name: "yoimiya",
+        img: "url('https://i.imgur.com/vi0IUU3.png')",
+        hp: 10164,
+        atk: 323,
+        def: 615,
+        rate: 24.2,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "aloy",
+        img: "url('https://i.imgur.com/GfkoAwV.png')",
+        hp: 10899,
+        atk: 234,
+        def: 676,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "amber",
+        img: "url('https://i.imgur.com/Pqk8t1m.png')",
+        hp: 9461,
+        atk: 223,
+        def: 601,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "diona",
+        img: "url('https://i.imgur.com/2WMcmnO.png')",
+        hp: 9570,
+        atk: 212,
+        def: 601,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "fischl",
+        img: "url('https://i.imgur.com/TB0tlZS.png')",
+        hp: 10164,
+        atk: 244,
+        def: 594,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "ganyu",
+        img: "url('https://i.imgur.com/GxYfIKD.png')",
+        hp: 9797,
+        atk: 335,
+        def: 630,
+        rate: 5,
+        cdmg: 88.4,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "gorou",
+        img: "url('https://i.imgur.com/WNqRmua.png')",
+        hp: 9570,
+        atk: 183,
+        def: 648,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "sara",
+        img: "url('https://i.imgur.com/uIpoo2m.png')",
+        hp: 9570,
+        atk: 195,
+        def: 628,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "child",
+        img: "url('https://i.imgur.com/9rYyGe8.png')",
+        hp: 13103,
+        atk: 301,
+        def: 815,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "venti",
+        img: "url('https://i.imgur.com/NfRP3Gt.png')",
+        hp: 10531,
+        atk: 263,
+        def: 669,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "barbara",
+        img: "url('https://i.imgur.com/QEmWAEn.png')",
+        hp: 9787,
+        atk: 159,
+        def: 669,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "klee",
+        img: "url('https://i.imgur.com/WXGzcq2.png')",
+        hp: 10287,
+        atk: 311,
+        def: 615,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "lisa",
+        img: "url('https://i.imgur.com/Te2rrJy.png')",
+        hp: 9570,
+        atk: 232,
+        def: 573,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "mona",
+        img: "url('https://i.imgur.com/iM8AQTR.png')",
+        hp: 10409,
+        atk: 287,
+        def: 653,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "ning",
+        img: "url('https://i.imgur.com/XeluR9w.png')",
+        hp: 9787,
+        atk: 212,
+        def: 573,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "kokomi",
+        img: "url('https://i.imgur.com/wy98HJy.png')",
+        hp: 13471,
+        atk: 234,
+        def: 657,
+        rate: -95,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "sucrose",
+        img: "url('https://i.imgur.com/KEgQqJF.png')",
+        hp: 9244,
+        atk: 170,
+        def: 70,
+        rate: 5,
+        cdmg: 5,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "yae",
+        img: "url('https://i.imgur.com/dQoUDeF.png)",
+        hp: 10372,
+        atk: 340,
+        def: 569,
+        rate: 24.2,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "yanfei",
+        img: "url('https://i.imgur.com/EyNhoUZ.png')",
+        hp: 9352,
+        atk: 240,
+        def: 587,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "hutao",
+        img: "url('https://i.imgur.com/bDBguiL.png')",
+        hp: 15552,
+        atk: 106,
+        def: 876,
+        rate: 5,
+        cdmg: 88.4,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "raiden",
+        img: "url('https://i.imgur.com/aAP29vs.png')",
+        hp: 12907,
+        atk: 337,
+        def: 789,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "rosaria",
+        img: "url('https://i.imgur.com/8lqUwfm.png')",
+        hp: 12289,
+        atk: 240,
+        def: 710,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "shenhe",
+        img: "url('https://i.imgur.com/YaLIll7.png')",
+        hp: 12993,
+        atk: 304,
+        def: 830,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "thoma",
+        img: "url('https://i.imgur.com/m7KGge1.png')",
+        hp: 10331,
+        atk: 202,
+        def: 751,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "xiangling",
+        img: "url('https://i.imgur.com/gOq7q8L.png')",
+        hp: 10875,
+        atk: 225,
+        def: 669,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "xiao",
+        img: "url('https://i.imgur.com/A4D0cxB.png')",
+        hp: 12736,
+        atk: 349,
+        def: 799,
+        rate: 24.2,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "yunjin",
+        img: "url('https://i.imgur.com/ZR4SSU0.png')",
+        hp: 10657,
+        atk: 191,
+        def: 734,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "zhongli",
+        img: "url('https://i.imgur.com/7mcooYp.png')",
+        hp: 14695,
+        atk: 251,
+        def: 736,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "itto",
+        img: "url('https://i.imgur.com/IIhyQuM.png')",
+        hp: 12858,
+        atk: 227,
+        def: 959,
+        rate: 24.2,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "beidou",
+        img: "url('https://i.imgur.com/R8CiFW8.png')",
+        hp: 13050,
+        atk: 225,
+        def: 648,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "chongyun",
+        img: "url('https://i.imgur.com/AYPqsb2.png')",
+        hp: 10984,
+        atk: 223,
+        def: 648,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "cringe",
+        img: "url('https://i.imgur.com/iGE8k5t.png')",
+        hp: 12981,
+        atk: 335,
+        def: 784,
+        rate: 24.2,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "eula",
+        img: "url('https://i.imgur.com/ZYuAzRG.png')",
+        hp: 13226,
+        atk: 342,
+        def: 751,
+        rate: 5,
+        cdmg: 88.4,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "noelle",
+        img: "url('https://i.imgur.com/IIBkn3G.png')",
+        hp: 12071,
+        atk: 191,
+        def: 799,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "razor",
+        img: "url('https://i.imgur.com/ZOiiEMM.png')",
+        hp: 11962,
+        atk: 234,
+        def: 751,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "sayu",
+        img: "url('https://i.imgur.com/iOFxpaf.png')",
+        hp: 11854,
+        atk: 224,
+        def: 745,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "xinyan",
+        img: "url('https://i.imgur.com/EUOAi0G.png')",
+        hp: 11201,
+        atk: 249,
+        def: 799,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "albedo",
+        img: "url('https://i.imgur.com/5XAMGkA.png')",
+        hp: 13226,
+        atk: 251,
+        def: 876,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "bennett",
+        img: "url('https://i.imgur.com/APjaVZ7.png')",
+        hp: 12397,
+        atk: 191,
+        def: 771,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "jean",
+        img: "url('https://i.imgur.com/1unHBp6.png')",
+        hp: 14695,
+        atk: 239,
+        def: 769,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "kazuha",
+        img: "url('https://i.imgur.com/K5qz5sW.png')",
+        hp: 13348,
+        atk: 297,
+        def: 807,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "kaeya",
+        img: "url('https://i.imgur.com/IgeaLEX.png')",
+        hp: 11636,
+        atk: 223,
+        def: 792,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "ayaka",
+        img: "url('https://i.imgur.com/L2FssfY.png')",
+        hp: 12858,
+        atk: 342,
+        def: 784,
+        rate: 5,
+        cdmg: 88.4,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "ayato",
+        img: "url('https://i.imgur.com/AUH78ff.png')",
+        hp: 13715,
+        atk: 299,
+        def: 769,
+        rate: 5,
+        cdmg: 88.4,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "keqing",
+        img: "url('https://i.imgur.com/YGyZdIA.png')",
+        hp: 13103,
+        atk: 323,
+        def: 799,
+        rate: 5,
+        cdmg: 88.4,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "qiqi",
+        img: "url('https://i.imgur.com/akXk0Ic.png')",
+        hp: 12368,
+        atk: 287,
+        def: 922,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "traveler",
+        img: "url('https://i.imgur.com/Yd0i1r7.png')",
+        hp: 10875,
+        atk: 212,
+        def: 683,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "xingqiu",
+        img: "url('https://i.imgur.com/OEbnWw5.png')",
+        hp: 10222,
+        atk: 202,
+        def: 758,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "yelan",
+        img: "url('https://i.imgur.com/8VMuGmk.png')",
+        hp: 0,
+        atk: 0,
+        def: 0,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "kuki",
+        img: "url('https://i.imgur.com/7l1zhx4.png')",
+        hp: 0,
+        atk: 0,
+        def: 0,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    },
+    {
+        name: "heizou",
+        img: "url('https://i.imgur.com/MDVvaYL.png')",
+        hp: 0,
+        atk: 0,
+        def: 0,
+        rate: 5,
+        cdmg: 50,
+        em: 0,
+        er: 100
+    }
+];
+
+
+
+function calc_stats()
+{
+    // ----- HP ----- //
+    var base_hp = parseFloat(document.getElementById("basehp").value);
+    var hp_pc   = parseFloat(document.getElementById("hppc").value);
+    var hp_flat = parseFloat(document.getElementById("hpflat").value);
+
+    // ----- ATK ----- //
+    var chr_base = parseFloat(document.getElementById("chrbase").value);
+    var wpn_base = parseFloat(document.getElementById("wpnbase").value);
+    var atk_pc   = parseFloat(document.getElementById("atkpc").value);
+    var atk_flat = parseFloat(document.getElementById("atkflat").value);
+
+    // ----- DEF ----- //
+    var base_def = parseFloat(document.getElementById("basedef").value);
+    var def_pc   = parseFloat(document.getElementById("defpc").value);
+    var def_flat = parseFloat(document.getElementById("defflat").value);
+
+    // ----- CRIT RATE ----- //
+    var base_rate  = parseFloat(document.getElementById("baserate").value);
+    var crit_pc = parseFloat(document.getElementById("ratepc").value);
+
+    // ----- CRIT DMG ----- //
+    var base_cdmg  = parseFloat(document.getElementById("basecdmg").value);
+    var cdmg_pc = parseFloat(document.getElementById("cdmgpc").value);
+    
+    // ----- EM ----- //
+    var base_em  = parseFloat(document.getElementById("baseem").value);
+    var em_extra = parseFloat(document.getElementById("empc").value);
+
+    // ----- ER ----- //
+    var base_er  = parseFloat(document.getElementById("baseer").value);
+    var er_extra = parseFloat(document.getElementById("erpc").value);
+
+    // ----- CONVERT PERCENTAGE TO DECIMAL ----- //
+    hp_pc = hp_pc / 100;
+    atk_pc = atk_pc / 100;
+    def_pc = def_pc / 100;
+
+    // ----- STAT FORMULAS ----- //
+    var hp = base_hp * (1.0 + hp_pc) + hp_flat;
+    var atk = (chr_base + wpn_base) * (1.0 + atk_pc) + atk_flat;
+    var def = base_def * (1.0 + def_pc) + def_flat;
+    var crate = base_rate + crit_pc;
+    var cdmg = base_cdmg + cdmg_pc;
+    var em = base_em + em_extra;
+    var er = base_er + er_extra;
+
+
+    // ----- CHANGE NUMBERS ON SCREEN----- //
+    document.getElementById("hp_result").innerHTML = Math.round(hp);
+    document.getElementById("atk_result").innerHTML = Math.round(atk);
+    document.getElementById("def_result").innerHTML = Math.round(def);
+    document.getElementById("rate_result").innerHTML = crate + "%"; 
+    document.getElementById("cdmg_result").innerHTML = cdmg + "%"; 
+    document.getElementById("em_result").innerHTML = em;
+    document.getElementById("er_result").innerHTML = er + "%";
+}
+
+function change_base()
+{
+    var x = document.getElementById("chrsel").value;
+    for(let i = 0; i < chr_list.length; i++)
+    {
+        if(chr_list[i].name === x)
+        {
+            document.getElementById("basehp").value = chr_list[i].hp;
+            document.getElementById("chrbase").value = chr_list[i].atk;
+            document.getElementById("basedef").value = chr_list[i].def;
+            document.getElementById("baserate").value = chr_list[i].rate;
+            document.getElementById("basecdmg").value = chr_list[i].cdmg;
+            document.getElementById("baseem").value = chr_list[i].em;
+            document.getElementById("baseer").value = chr_list[i].er;
+            document.getElementById("chrimg").style.backgroundImage = "linear-gradient(rgba(31, 31, 31,0.5), rgba(31, 31, 31,0.8))," + chr_list[i].img;
+        }
+    }
+}
+
+
